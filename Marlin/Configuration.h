f666087ca21ba4fa2563f0b7e612e675e93b8590
Jim Brown <jb.github@rcairgallery.com>
2021-10-11 14:59:43 -0400

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1307,7 +1307,7 @@
   #endif
 #endif
 
-#define DEFAULT_EJERK    2.5  // May be used by Linear Advance
+#define DEFAULT_EJERK    10  // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
